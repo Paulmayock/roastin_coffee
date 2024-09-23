19bd5f129bded0a93cde13cb9bd8cf490d41d6c3
Paul Mayock <paulmayock@hotmail.com>
2024-09-23 12:59:54 +0000

    first deployment

--- roastin_coffee/settings.py
+++ roastin_coffee/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-ptj1dk3e_c_fsz3$d5@0onl@4-b#xx4h3j^3!ul%=rt5mpq45h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-paulmayock-roastincoffe-h6ym9qelhf3.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-paulmayock-roastincoffe-h6ym9qelhf3.ws.codeinstitute-ide.net', 'localhost']
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-paulmayock-roastincoffe-h6ym9qelhf3.ws.codeinstitute-ide.net']
 
@@ -105,12 +106,18 @@ WSGI_APPLICATION = 'roastin_coffee.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
