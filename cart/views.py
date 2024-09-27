4aef941f360e4fd81608a52a80f26f20605798c5
Paul Mayock <paulmayock@hotmail.com>
2024-09-27 20:42:16 +0000

    added adjust cart and cart views, and calc subtotal template

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -21,3 +21,36 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)    
+
+def adjust_cart(request, item_id):
+    """ adjust the the shopping cart amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        del cart[item_id]
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))  
+
+def remove_from_cart(request, item_id):
+    """ remove item from cart"""
+
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        del cart[item_id]
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))          
