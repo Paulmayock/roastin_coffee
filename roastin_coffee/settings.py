1ad44fe5ef4faf280f27937f3eb46a5d6214e1b7
Paul Mayock <paulmayock@hotmail.com>
2024-10-01 10:37:48 +0000

    created blog model and templates

--- roastin_coffee/settings.py
+++ roastin_coffee/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'checkout',
     'crispy_forms',
     'profiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -199,4 +200,16 @@ DEFAULT_FROM_EMAIL = 'Roastincoffee@example.com'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'roastincoffee@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
