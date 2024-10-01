436aa112e4789e650a24404a93973118d48737b1
Paul Mayock <paulmayock@hotmail.com>
2024-10-01 10:51:26 +0000

    added post and commet classes to blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+from profiles.models import UserProfile
 
-# Create your models here.
+slug = models.SlugField()
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="blog_posts")
+    created_on = models.DateField(auto_now_add=True)
+    featured_image = models.ImageField(null=True, blank=True)
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(UserProfile, related_name="post_likes",
+                                   blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    prepopulated_fields = {'slug': slugify}
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name="user_comments")
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 
-# Create your views here.
+@login_required
+def add_post(request):
+    """
+    View to add a blog post.
+    """
+    print(request.user)
+    post_form = PostForm(request.POST, request.FILES)
+
+    if request.method == 'POST':
+        if post_form.is_valid():
+            post_form = post_form.save(commit=False)
+            post_form.title = post_form.title
+            post_form.author = request.user
+            post_form.status = 1
+            post_form.save()
+            return redirect(reverse('blog'))
+
+    context = {
+        'post_form': post_form
+    }
+
+    return render(request, 'blog/add_post.html', context)
